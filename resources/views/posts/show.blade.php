117e8e9f2a972876cc1047b06d084fd67a6c4495
albertgc <gualbertpcuizav@gmail.com>
2020-10-17 20:43:12 -0400

    Seo dinsmic tags add

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,17 @@
-<h1>{{ $post->title }} | {{ $post->category->name }}</h1>
-<hr>
-<p>{{ $post->excerpt }}</p>
-<p>{{ $post->body }}</p>
-<small>{{ $post->published_at->format('M d') }}</small><br>
-@foreach ($post->tags as $tag)
-    #<span>{{ $tag->name }}</span>
-@endforeach
+@extends('layout')
+
+@section('title', $post->title)
+@section('description', $post->excerpt)
+
+@section('content')
+
+    <h1>{{ $post->title }} | {{ $post->category->name }}</h1>
+    <hr>
+    <p>{{ $post->excerpt }}</p>
+    <p>{{ $post->body }}</p>
+    <small>{{ $post->published_at->format('M d') }}</small><br>
+    @foreach ($post->tags as $tag)
+        #<span>{{ $tag->name }}</span>
+    @endforeach
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
+@section('content')
+    
+    <ul>
+        @foreach ($posts as $post)
+            <li>
+                <h2>{{ $post->title }} | {{ $post->category->name }}</h2>
+                <p>{{ $post->excerpt }}</p>
+                <small>{{ $post->published_at->format('M d') }}</small><br>
+                @foreach ($post->tags as $tag)
+                    #<span>{{ $tag->name }}</span>
+                @endforeach
+                <a href="{{ route('posts.show', $post) }}">Leer todo</a>
+            </li>
+        @endforeach
+    </ul>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body class="antialiased">
-        <ul>
-            @foreach ($posts as $post)
-                <li>
-                    <h2>{{ $post->title }} | {{ $post->category->name }}</h2>
-                    <p>{{ $post->excerpt }}</p>
-                    <small>{{ $post->published_at->format('M d') }}</small><br>
-                    @foreach ($post->tags as $tag)
-                        #<span>{{ $tag->name }}</span>
-                    @endforeach
-                    <a href="{{ route('posts.show', $post) }}">Leer todo</a>
-                </li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@endsection
