89be0180e4a6fde3a65ff42069ad02ce7c69ec17
albertgc <gualbertpcuizav@gmail.com>
2020-10-21 22:29:12 -0400

    show post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,13 +5,28 @@
 
 @section('content')
 
-    <h1>{{ $post->title }} | {{ $post->category->name }}</h1>
-    <hr>
-    <p>{{ $post->excerpt }}</p>
-    <p>{{ $post->body }}</p>
-    <small>{{ $post->published_at->format('M d') }}</small><br>
-    @foreach ($post->tags as $tag)
-        #<span>{{ $tag->name }}</span>
-    @endforeach
+    <div class="box w-full md:w-1/2 mx-auto bg-white mb-4 py-4 pl-6 shadow-md rounded-sm">
+        <div class="flex justify-between">
+            <span class="tracking-widest">{{ $post->published_at->format('M d') }}</span>
+            <span class="bg-green-500 w-1/6 shadow-md text-center text-gray-300 rounded-l-lg">{{ $post->category->name }}</span>
+        </div>
+        <div class="pr-6">
+            <div class="my-4">
+                <h2 class="text-4xl tracking-tight">{{ $post->title }}</h2>
+            </div>
+            <hr>
+            <div class="my-4">
+                <p class="leading-relaxed mb-4">{{ $post->body }}</p>
+                <div class="flex justify-between">
+                    <div></div>
+                    <div>
+                        @foreach ($post->tags as $tag)
+                            <span class="text-gray-600">#{{ $tag->name }}</span>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
